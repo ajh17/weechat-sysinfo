8dcfc01b347c8b3afee4d9137a0bac2220a9db61
Akshay Hegde <akshay.hegde17@gmail.com>
2014-05-12 23:33:47 -0700

    Several changes for sysinfo.
    - Use with statement for os.popen()
    - Further simplified some code.
    - Added docstrings

--- sysinfo.py
+++ sysinfo.py
@@ -17,6 +17,7 @@ SCRIPT_VERSION = '1.3'
 SCRIPT_LICENSE = 'GPL3'
 SCRIPT_DESC = 'Prints out system information out to the channel'
 SCRIPT_COMMAND = "sysinfo"
+BYTES = 1073741824
 
 weechat.register(
     SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
@@ -28,67 +29,94 @@ hook = weechat.hook_command(
 
 
 def model_info():
-    plist_path = '~/.weechat/python/sysinfo/data/MacintoshModels.plist'
-    mac_name = os.popen(
-        'defaults read ' + plist_path + ' | '
+    '''
+    Get this Mac's model name
+    '''
+    model_command = (
+        'defaults read ~/.weechat/python/sysinfo/data/MacintoshModels.plist |'
         'grep `sysctl -n hw.model` | awk -F\\\" {\'print $4\'}'
-    ).readlines()[0].rstrip()
-    return "Model: {}".format(mac_name)
+    )
+    with os.popen(model_command) as mac_name:
+        return "Model: {}".format(mac_name.readlines()[0].rstrip())
 
 
 def cpu_info():
+    '''
+    Get the processor information, including the number of cores of this
+    machine.
+    '''
     cpu_command = (
         "sysctl machdep.cpu.brand_string |"
         "awk '{print $2,$3,$4,$5,$6,$7,$8,$9}'"
     )
-    cpu = os.popen(cpu_command).readlines()[0].rstrip()
-    cores = os.popen("sysctl -n machdep.cpu.core_count").readlines()[0]
-    cpu = re.sub("(\(R\)|\(TM\))", "", cpu)
-    return cpu + " (" + cores.rstrip() + " cores)"
+    with os.popen(cpu_command) as cpu:
+        with os.popen("sysctl -n machdep.cpu.core_count") as cores:
+            cpu = re.sub("(\(R\)|\(TM\))", "", cpu.readlines()[0].rstrip())
+            return "{} ({} cores)".format(cpu, cores.readlines()[0].rstrip())
 
 
 def ram_info():
-    memory_command = "sysctl -n hw.memsize"
-    mem = os.popen(memory_command).readlines()[0].rstrip()
-    mem = int(mem) / 1048576 / 1024
-    return "Memory: " + str(float(mem)) + " GB"
+    '''
+    Get the Memory size of this machine.
+    '''
+    with os.popen("sysctl -n hw.memsize") as mem:
+        mem = float(int(mem.readlines()[0].rstrip()) / BYTES)
+        return "Memory: {} GB".format(mem)
 
 
 def os_info():
+    '''
+    Get the OS information including the Build number.
+    '''
     version_command = (
         "system_profiler SPSoftwareDataType | grep -Po '(?<=: )OS X.*'"
     )
-    version = os.popen(version_command).readlines()[0].rstrip()
-    return version
+    with os.popen(version_command) as version:
+        return version.readlines()[0].rstrip()
 
 
 def gpu_info():
+    '''
+    Get the GPU information of this machine.
+    NOTE: Currently this only works with Macs with dual GPUs.
+    '''
     gpu_command = (
         "system_profiler SPDisplaysDataType | egrep 'Ch|VR' | "
         "grep -Po '(?<=: ).*' | paste -s -d ' ' - |"
         "sed -E 's/([0-9]+ MB)/(\\1)/g' | sed 's/)/) +/'"
     )
-    gpu = os.popen(gpu_command).readlines()[0].rstrip()
-    return gpu
+    with os.popen(gpu_command) as gpu:
+        return gpu.readlines()[0].rstrip()
 
 
 def uptime_info():
-    uptime = os.popen('uptime | grep -PZo "(?<=up )[^,]*"').readlines()[0]
-    return "Uptime: " + uptime.rstrip()
+    '''
+    Get the amount of time that this machine has been on for, i.e. the uptime.
+    '''
+    with os.popen('uptime | grep -PZo "(?<=up )[^,]*"') as uptime:
+        return "Uptime: {}".format(uptime.readlines()[0].rstrip())
 
 
 def load_info():
-    return "Average Load: " + str(psutil.cpu_percent()) + "%"
+    '''
+    Get the current average CPU load of this machine.
+    '''
+    return "Average Load: {}%".format(psutil.cpu_percent())
 
 
 def client_info():
+    '''
+    Get the IRC Client version information.
+    '''
     return "Client: WeeChat {}".format(weechat.info_get("version", ""))
 
 
 def get_sysinfo(data, buffer, args):
+    '''
+    Gets various system information related to this machine.
+    '''
     item_list = [model_info, cpu_info, ram_info, gpu_info, uptime_info,
                  load_info, os_info, client_info]
-
     result = " ･ ".join([func() for func in item_list])
     weechat.command(buffer, result)
 
